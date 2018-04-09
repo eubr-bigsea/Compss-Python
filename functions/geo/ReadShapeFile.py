c279922ac856f038502e66ff95ab26b360fb228d
Lucas Ponce <lucasmsp@gmail.com>
2018-04-09 14:45:45 -0300

    adding geo operations

--- functions/geo/ReadShapeFile.py
+++ functions/geo/ReadShapeFile.py
@@ -11,80 +11,85 @@ from pycompss.api.task import task
 from pycompss.api.parameter import *
 
 
-# @task(returns=list)
-def ReadShapeFileOperation(settings, numFrag):
-    """ReadShapeFileOperation.
+class ReadShapeFileOperation(object):
 
-    :param settings:    A dictionary that contains:
-        - host:         The host of the Namenode HDFS; (default, default)
-        - port:         Port of the Namenode HDFS; (default, 0)
-        - shp_path:     Path to the shapefile (.shp)
-        - dbf_path:     Path to the shapefile (.dbf)
-        - polygon:      Alias to the new column to store the
-                        polygon coordenates (default, 'points');
-        - attributes:   List of attributes to keep in the dataframe,
-                        empty to use all fields;
-        - lat_long:     True  if the coordenates is (lat,log),
-                        False if is (long,lat). Default is True;
-    Note: pip install pyshp
-    """
-    import shapefile
-    from io import BytesIO, StringIO
-    import hdfs_pycompss.hdfsConnector as hdfs
+    def __init__(self):
+        pass
 
-    polygon = settings.get('polygon', 'points')
-    lat_long = settings.get('lat_long', True)
-    header = settings.get('attributes', [])
+    def action(self, settings, numFrag):
+        """ReadShapeFileOperation.
 
-    settings['path'] = settings['shp_path']
-    Blks = hdfs.findNBlocks(settings, 1)
-    shp_path = hdfs.readBinary(Blks[0])
-    settings['path'] = settings['dbf_path']
-    Blks = hdfs.findNBlocks(settings, 1)
-    dbf_path = hdfs.readBinary(Blks[0])
+        :param settings: A dictionary that contains:
+            - host: The host of the Namenode HDFS; (default, default)
+            - port: Port of the Namenode HDFS; (default, 0)
+            - shp_path: Path to the shapefile (.shp)
+            - dbf_path: Path to the shapefile (.dbf)
+            - polygon: Alias to the new column to store the
+                polygon coordenates (default, 'points');
+            - attributes: List of attributes to keep in the dataframe,
+                empty to use all fields;
+            - lat_long: True  if the coordenates is (lat,log),
+                False if is (long,lat). Default is True;
+        Note: pip install pyshp
+        """
+        import shapefile
+        from io import BytesIO, StringIO
+        import hdfs_pycompss.hdfsConnector as hdfs
 
-    shp_io = BytesIO(shp_path)
-    dbf_io = BytesIO(dbf_path)
+        polygon = settings.get('polygon', 'points')
+        lat_long = settings.get('lat_long', True)
+        header = settings.get('attributes', [])
 
-    shp_object = shapefile.Reader(shp=shp_io, dbf=dbf_io)
-    records = shp_object.records()
-    sectors = shp_object.shapeRecords()
+        settings['path'] = settings['shp_path']
+        Blks = hdfs.findNBlocks(settings, 1)
+        shp_path = hdfs.readBinary(Blks[0])
+        settings['path'] = settings['dbf_path']
+        Blks = hdfs.findNBlocks(settings, 1)
+        dbf_path = hdfs.readBinary(Blks[0])
 
-    fields = {}  # column: position
-    for i, f in enumerate(shp_object.fields):
-        fields[f[0]] = i
-    del fields['DeletionFlag']
+        shp_io = BytesIO(shp_path)
+        dbf_io = BytesIO(dbf_path)
 
-    if len(header) == 0:
-        header = [f for f in fields]
+        shp_object = shapefile.Reader(shp=shp_io, dbf=dbf_io)
+        records = shp_object.records()
+        sectors = shp_object.shapeRecords()
 
-    num_fields = [fields[f] for f in header]  # position of each selected field
+        fields = {}  # column: position
+        for i, f in enumerate(shp_object.fields):
+            fields[f[0]] = i
+        del fields['DeletionFlag']
 
-    header.append(polygon)
-    data = []
-    for i, sector in enumerate(sectors):
-        attributes = []
-        r = records[i]
-        for t in num_fields:
-            attributes.append(r[t-1])
+        if len(header) == 0:
+            header = [f for f in fields]
 
-        points = []
-        for point in sector.shape.points:
-            if lat_long:
-                points.append([point[1], point[0]])
-            else:
-                points.append([point[0], point[1]])
-        attributes.append(points)
-        data.append(attributes)
+        # position of each selected field
+        num_fields = [fields[f] for f in header]
 
-    geo_data = pd.DataFrame(data, columns=header)
+        header.append(polygon)
+        data = []
+        for i, sector in enumerate(sectors):
+            attributes = []
+            r = records[i]
+            for t in num_fields:
+                attributes.append(r[t-1])
 
-    # forcing pandas to infer the dtype
-    tmp = geo_data.drop(polygon, axis=1)
-    vector = geo_data[polygon]
-    buff = tmp.to_csv(index=False)
-    geo_data = pd.read_csv(StringIO(unicode(buff)))
-    geo_data[polygon] = vector
+            points = []
+            for point in sector.shape.points:
+                if lat_long:
+                    points.append([point[1], point[0]])
+                else:
+                    points.append([point[0], point[1]])
+            attributes.append(points)
+            data.append(attributes)
 
-    geo_data = np.array_split(geo_data, numFrag)
-    return geo_data
+        geo_data = pd.DataFrame(data, columns=header)
+
+        # forcing pandas to infer the dtype
+        tmp = geo_data.drop(polygon, axis=1)
+        vector = geo_data[polygon]
+        buff = tmp.to_csv(index=False)
+        geo_data = pd.read_csv(StringIO(unicode(buff)))
+        geo_data[polygon] = vector
+
+        geo_data = np.array_split(geo_data, numFrag)
+        return geo_data
